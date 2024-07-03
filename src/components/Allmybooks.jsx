9279f9227e61a39dfd0c2ad3261b2802cc4dea32
samu2704 <samuele.calabrese@studenti.unisalento.it>
2024-07-03 20:56:43 +0200

    aggiunta  barra di ricerca e bordo rosso

--- src/components/Allmybooks.jsx
+++ src/components/Allmybooks.jsx
@@ -1,14 +1,46 @@
 import books from "../books/history.json"
-import Mycard from "./Mycard"
 import "./Allmybooks.css"
 import { Container } from "react-bootstrap"
-import {Row} from "react-bootstrap"
-function Allmybooks(){
-    return(
-        <div className="d-flex flex-wrap justify-content-center">
-            <Container />
-            <Row />
-{books.map(b=><Mycard key={b.asin} tuttidettagli={b}/>)}
+import { Row } from "react-bootstrap"
+import SingleBook from "./SingleBook"
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { useState } from "react"
+
+
+
+function Allmybooks() {
+
+    
+
+    const  [search, setSearch]= useState(" ")
+    const [resultSearch, setResultSearch] = useState(books)
+    const handleSearch = (e) =>{
+        setSearch(e.target.value)
+        const resultTemp = books.filter((book) =>{
+           return book.title.toLowerCase().includes(e.target.value.toLowerCase())
+        })
+        setResultSearch(resultTemp)
+    } 
+    return (
+        <div >
+
+            <InputGroup className="mb-3">
+                <Form.Control
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                    onChange={handleSearch}
+                />
+              
+            </InputGroup>
+
+
+            <Container>
+                <Row>
+                    {resultSearch.map(b => <SingleBook key={b.asin} tuttidettagli={b} />)}
+                </Row>
+            </Container>
         </div>
     )
 
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,12 +1,19 @@
 import { Card, Col  } from "react-bootstrap";
-import "./Mycard.css"
+import "./SingleBook.css"
+import { useState } from "react";
 
-function Mycard({tuttidettagli}) {
-  
+
+function SingleBook({tuttidettagli}) {
+    const [border, setBorder] = useState(false)
+  const handleClick = () =>{
+    setBorder(!border)
+  };
     return (
       
       <Col xs={12} sm={6} md={3}>
-      <Card className="book h-100 mb-4">
+      <Card className={border ? "mb-3 bordeRed" :" mb-3"}
+      onClick={handleClick} >
+      
         <Card.Img variant="top" style={{height:""}} src={tuttidettagli.img} />
         <Card.Body>
           <Card.Title className="title">{tuttidettagli.title} </Card.Title>
@@ -19,4 +26,4 @@ function Mycard({tuttidettagli}) {
     );
   }
   
-  export default Mycard;
+  export default SingleBook;
